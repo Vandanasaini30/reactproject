cfc2c5f8729ef49e940ab227640dee76c68e2977
vandana solanki <103331349+Vandanasaini30@users.noreply.github.com>
2023-09-25 18:22:04 +0530

    Create reactexpenseitem.js

---
+++ reactexpenseitem.js
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h1>food rs 10</h1>
+      <div>
+        <h1>petrol Rs 100</h1>
+      </div>
+      <div>
+        <h1>movie Rs 200</h1>
+      </div>
+    </div>
+  );
+}
+export default ExpenseItem;
